43b89fe562a4abad927fa3f0687cce013dccf59a
Ari <ari@miniasp.com>
2022-08-21 17:25:30 +0800

    add tailwindcss

---
+++ Client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.razor", "**/*.cshtml", "**/*.html", "**/*.cshtml"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ Tailwindcss.bat
@@ -0,0 +1,2 @@
+cd Client
+npx tailwindcss -i .\wwwroot\css\tailwindcss\input.css -o .\wwwroot\css\tailwindcss\tailwind.css --watch
