afac43042205525054580e66735775cb2a80d916
Ari <ari@miniasp.com>
2022-08-31 16:46:08 +0800

    文章清單排版

---
+++ Model/ArticleIntroductionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Model;
+
+public class ArticleIntroductionModel
+{
+    public string? Title { get; set; }
+
+    public string? NickName { get; set; }
+
+    public DateTime? DisplayDate { get; set; }
+
+    public string[]? Hints { get; set; }
+}
