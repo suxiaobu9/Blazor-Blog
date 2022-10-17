7f73cc54d3668645cfa9a514f1012b500f8bb200
Ari <arisuokayokay@gmail.com>
2022-10-17 12:46:35 +0800

    改圖片、引用css以及即時更新網站

---
+++ Blazor.Blog/tailwind.config.js
@@ -0,0 +1,12 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["**/*.razor", "**/*.cshtml", "**/*.html", "**/*.cshtml"],
+    theme: {
+        extend: {
+            display: {
+                'none': 'hidden'
+            }
+        },
+    },
+    plugins: [],
+};
